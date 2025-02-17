3d57af834cdfad5fe9ca024e672fc8a31c48c3c1
Alec Kirkley <alec.w.kirkley@gmail.com>
2025-02-17 17:26:17 +0800

    Update quantity_diversity.py

--- hina/individual/quantity_diversity.py
+++ hina/individual/quantity_diversity.py
@@ -30,38 +30,29 @@ def get_bipartite(df,col1,col2):
     
     return G
 
-def quantity_and_diversity(df,student_col,task_col,weight_col):
+def quantity_and_diversity(df,student_col,task_col):
     """
     compute quantity and diversity measures of Feng et al 2024 for students in student_col
     inputs:
         df is dataframe
         student_col is column name for student identifiers
         task_col is column name for tasks
-        weight_col is column name for weights of tasks
     returns:
-        quantity and diversity measures for student nodes with respect to indicated tasks and weights,
+        quantity and diversity measures for student nodes with respect to indicated tasks,
             in form of dictionaries with {node name: quantity} and {node name: diversity}
     """
     G = get_bipartite(df,student_col,task_col)
-    weights_dict = dict(df[[task_col,weight_col]].values)
-    W = sum(weights_dict.values())
+    W = sum([e[-1] for e in G])
     N2 = len(set([e[1] for e in G]))
     
-    quantities = {}
+    quantities,out_weights = {},{}
     for e in G:
-        i,j = e[0],e[1]
+        
+        i,j,wij = e
+        
         if not(i in quantities): quantities[i] = 0
-        quantities[i] += weights_dict[j]/W
-
-    degs = {}
-    for e in G:
-        i = e[0]
-        if not(i in degs): degs[i] = 0
-        degs[i] += e[-1]
-
-    out_weights = {}
-    for e in G:
-        i,j,w = e
+        quantities[i] += wij/W
+    
         if not(i in out_weights): out_weights[i] = {}
         if not(j in out_weights[i]): out_weights[i][j] = 0
         out_weights[i][j] += w
@@ -69,7 +60,7 @@ def quantity_and_diversity(df,student_col,task_col,weight_col):
     diversities = {}
     for i in out_weights:
         wi = sum(out_weights[i].values())
-        diversities[i] = -sum([(out_weights[i][j]/wi)*np.log(out_weights[i][j]/wi) for j in out_weights[i]])
+        diversities[i] = -sum((out_weights[i][j]/wi)*np.log(out_weights[i][j]/wi) for j in out_weights[i])
         diversities[i] /= np.log(N2)
     
     return quantities,diversities
